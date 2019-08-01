180772f0cfe9232ac73e161cb7fc36ca5bf6eaf1
Quentin Wach <quentin.wach@googlemail.com>
2019-08-01 16:52:30 +0200

    Program läuft

--- Skyhook/README.md
+++ Skyhook/README.md
@@ -17,8 +17,8 @@ Deadline:   10. Oktober
 ### TODO
 1)  [ ] vereinfachte Planetebahnen (Sonne, Erde Mars)
     1)  [X] Funktion Planetenbahn
-    2)  [ ] Planetenklasse
-    3)  [ ] Planetenbahnen aus Klasse
+    2)  [X] Planetenklasse
+    3)  [X] Planetenbahnen aus Klasse
     4)  [ ] Animation der Planetenbewegungen
 2)  [ ] Interaktion Raumschiff im Gravitationsfeld
 3)  [ ] Orbit Raumschiff
@@ -28,7 +28,13 @@ Deadline:   10. Oktober
 7)  [ ] Raumschiff mit Skyhook senden und auffangen zwischen Erde und Mars
 8)  [ ] Periodischer aumtomatischer Austausch von Raumschiffen zwischen Skyhooks auf versch. Planeten
 
----
-
+### Setup
 Geschrieben in Python3.5. Die nötigen Module finden sich in requirements.txt und lassen sich über pip install in einem virtualenv installieren. (Achtung: Derzeit viele überflüssige Module.)
 
+1. Installiere `Python3.5`
+2. Installiere `pip`
+2. Installiere `virutalenv`
+3. Erstelle eine virtuelle Umgebung mit Python3.5 und wähle dieses aus
+4. Lade den Projektordner herunter und wählen die dir im Terminal aus
+5. Installiere die nötigen Module mit `pip install -r requirements.txt` 
+6. Führe das Program mit `python run.py` aus
--- Skyhook/run.py
+++ Skyhook/run.py
@@ -1,51 +1,83 @@
 # Module
 import numpy as np
 import matplotlib.pyplot as plt
-import massen
 
-# Massen der Körper
+# Massen der Körper [kg]
 M_SUN = 0
 M_EARTH = 0
 M_MARS = 0 
+# Umlaufzeiten [Tage]
+UT_SUN = 1
+UT_EARTH = 365
+UT_MARS = 720
+# Abstände zur Sonne (relativ)
+D_SUN = 0
+D_EARTH = 1
+D_MARS = 1.5
+# Radien der Körper [m]
+R_SUN =     69634200000000
+R_EARTH =          6371001
+R_MARS =           3389500
 
 #-----------------------------------------------------------------------------
-class Planet():
-    def init(self, m, ut, r):
+class Planet:
+    def __init__(self, M, UT, D, R):
         """
         Masse: m
         Umlaufzeit: ut
-        Entfernung zur Sonne: r
+        Entfernung zur Sonne: d
+        Radius des Körpers: r
         """
-        self.m = m
-        self.ut = ut
-        self.r = r
+        self.m = M
+        self.ut = UT
+        self.d = D
+        self.r = R
+        
+        # Sammlung der Datenpunkte
+        self.X = []
+        self.Y = []
 
     # Planetenbahn
-    def planet_orbit(r, ut, t):
+    def planet_orbit(self, t):
         """
         t:  spez. Zeitpunkt
         """
-        pt = 2 * np.pi * t / T
-        x = R * np.sin(pt)
-        y = R* np.cos(pt)
+        self.pt = 2 * np.pi * t / self.ut
+        x = self.d * np.sin(self.pt)
+        y = self.d * np.cos(self.pt)
         return x, y
 
-    # Sammlung der Datenpunkte
-    x_past = []
-    y_past = []
+# Erstelle Planetenobjekte
+sun = Planet(M_SUN, UT_SUN, D_SUN, R_SUN)
+earth = Planet(M_EARTH, UT_EARTH, D_EARTH, R_EARTH)
+mars = Planet(M_MARS, UT_MARS, D_MARS, R_MARS)
 
+# Sammle die Positionen der Planeten im Zeitverlauf
+TIME = 1000
+for t in range(TIME):
+    # Sonne
+    #sun.X.append(sun.planet_orbit(t)[0])
+    #sun.Y.append(sun.planet_orbit(t)[1])
+    # Erde
+    earth.X.append(earth.planet_orbit(t)[0])
+    earth.Y.append(earth.planet_orbit(t)[1])    
+    # Mars
+    mars.X.append(mars.planet_orbit(t)[0])
+    mars.Y.append(mars.planet_orbit(t)[1])  
+    
+# Plotte die Bahnen
+#plt.plot(sun.X, sun.Y)
+plt.plot(earth.X, earth.Y)
+plt.plot(mars.X, mars.Y)
 
-
-
-
-
+plt.show()
 
 
 
 
 #-----------------------------------------------------------------------------
 # Animiere die Simulation
-
+"""
 def animate(i):
     # Koordinaten der drei Punkte
     thisx = [0, x1[i], x2[i]]
@@ -71,20 +103,6 @@ def animate(i):
 
     return line, dots, time_text
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #-----------------------------------------------------------------------------
 # Starte die Simulation
 if __name__ == '__main__':
@@ -93,3 +111,4 @@ if __name__ == '__main__':
     #anim.save('pendel.gif', dpi=80, writer='imagemagick') 
 
     plt.show()
+"""

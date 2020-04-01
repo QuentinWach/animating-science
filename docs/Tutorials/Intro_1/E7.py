d0a06f5426f1317985bdf47e04d70eb1e0137cc9
Quentin Wach <quentin.wach@googlemail.com>
2020-04-01 14:22:03 +0200

    + Axes graphic

--- docs/Tutorials/Intro_1/E7.py
+++ docs/Tutorials/Intro_1/E7.py
@@ -6,7 +6,7 @@ import matplotlib
 import numpy as np
 np.random.seed(42)
 
-fig = plt.figure(figsize=(16,4), dpi=300)
+fig = plt.figure(figsize=(8,2), dpi=300)
 
 
 ax1 = plt.Axes(fig=fig, rect=[0.51, 0.03, 0.48, 0.8], facecolor='white')
@@ -24,9 +24,11 @@ ax1.plot(x1,y1,"o-", color="lightgray", alpha=0.85)
 ax1.set_xlim([0,1])
 ax1.set_ylim([0,1])
 
-ax1.annotate("", xy=(0.99, 0.0), xytext=(0, 0),arrowprops=dict(arrowstyle="->"))
-ax1.annotate("", xy=(0.0, 0.975), xytext=(0, 0), arrowprops=dict(arrowstyle="->"), backgroundcolor="white")
-
 ax1.text(0.83,0.985,"Axes Container", color="black", size=10, horizontalalignment='center', backgroundcolor="white")
+ax1.text(0,0.93," ", color="black", size=3, backgroundcolor="white", zorder=10)
+ax1.text(0.963,0," ", color="black", size=3, backgroundcolor="white", zorder=10)
+
+plt.arrow(0, 0, 0, 0.828, head_width=0.03, head_length=0.06, lw=3, fc='k', ec='k', zorder=30)
+plt.arrow(0, 0, 0.9245, 0, head_width=0.07, head_length=0.025, lw=3, fc='k', ec='k', zorder=30)
 
 plt.savefig("E7.svg", bbox_inches="tight")

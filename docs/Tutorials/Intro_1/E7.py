54336b84cf25741041d6576facaa9a6513f9d75f
Quentin Wach <quentin.wach@googlemail.com>
2020-04-01 02:25:04 +0200

    adding figures

--- docs/Tutorials/Intro_1/E7.py
+++ docs/Tutorials/Intro_1/E7.py
@@ -5,33 +5,27 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
-fig = plt.figure(figsize=(8,2), dpi=300)
-f = plt.Axes(fig=fig, rect=[0,0,1,1], facecolor='lightgray')
-fig.add_axes(f)     
+fig = plt.figure(figsize=(16,4), dpi=300)
+
+
 ax1 = plt.Axes(fig=fig, rect=[0.51, 0.03, 0.48, 0.8], facecolor='white')
 fig.add_axes(ax1)
-ax2 = plt.Axes(fig=fig, rect=[0.01, 0.03, 0.48, 0.8], facecolor='white')
-fig.add_axes(ax2)
 ax1.set_xticks([]); ax1.set_yticks([])
-ax2.set_xticks([]); ax2.set_yticks([])
-f.set_xticks([]); f.set_yticks([])
-ax1.text(0.5, 0.5, "Axes Container" , va="center", ha="center")
-ax2.text(0.5, 0.5, "Axes Container" , va="center", ha="center")
 ax1.tick_params(labelbottom=False, labelleft=False)
-ax2.tick_params(labelbottom=False, labelleft=False)
+ax1.set_ylabel("Axis Container")
+ax1.set_xlabel("Axis Container")
+matplotlib.artist.getp(ax1)
 
 N = 50
-x1 = 0.021 * np.arange(N)
-y1 = 0.42 * np.random.rand(N) 
-x2 = 0.021* np.arange(N)
-y2 = 0.42 * np.random.rand(N)
-ax1.plot(x1,y1,"o-", color="lightgray", alpha=0.25)
-ax2.plot(x2,y2,"o-", color="lightgray", alpha=0.25)
+x1 = 0.021 * np.arange(0.3,N)
+y1 = 0.62 * np.random.rand(N) + 0.17 
+ax1.plot(x1,y1,"o-", color="lightgray", alpha=0.85)
 ax1.set_xlim([0,1])
 ax1.set_ylim([0,1])
-ax2.set_xlim([0,1])
-ax2.set_ylim([0,1])
 
-f.text(0.5,0.89,"Figure Container", color="black", size=10, horizontalalignment='center')
+ax1.annotate("", xy=(0.99, 0.0), xytext=(0, 0),arrowprops=dict(arrowstyle="->"))
+ax1.annotate("", xy=(0.0, 0.975), xytext=(0, 0), arrowprops=dict(arrowstyle="->"), backgroundcolor="white")
 
-plt.savefig("E6.svg", bbox_inches="tight")
+ax1.text(0.83,0.985,"Axes Container", color="black", size=10, horizontalalignment='center', backgroundcolor="white")
+
+plt.savefig("E7.svg", bbox_inches="tight")

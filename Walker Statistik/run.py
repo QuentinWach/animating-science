2275121fdf13b9be7f934978e7a6169ba239fd03
Quentin Wach <quentin.wach@googlemail.com>
2020-07-01 03:03:34 +0200

    .

--- Walker Statistik/run.py
+++ Walker Statistik/run.py
@@ -10,22 +10,22 @@ import numpy as np
 import math
 np.random.seed(14520)
 
-# Figure Setup
+# figure setup
 matplotlib.style.use('ggplot')
 fig, ax = plt.subplots(1, figsize=(6,2.7), dpi=300)
 
-# Parameter
-P = 0.3               # Schritt nach Links
-Q = 1 - P             # Schritt nach Rechts
-T = (10,100,1000)     # Schritte
-V = 10000              # Versuche pro Experiment
+# HYPERPARAMETER
+P = 0.3     # Schritt nach Links
+Q = 1 - P   # Schritt nach Rechts
+T = (10,100,1000)     # Zeitschritte
+E = 3000              # Experimente
 
-# Experimente
+# DATEN
+a = 0
 for t in T:
-
-    # Endpunkte [Schritte]
+    # Verteilung
     X = []
-    for v in range(V):
+    for e in range(E):
         x = 0       # Startpunkt
         for i in range(t):
             if np.random.random_sample() <= P:
@@ -33,32 +33,13 @@ for t in T:
             else:
                 x += 1
         X.append(x)
-    X = sorted(X)
-    
-    # Wahrscheinlichkeiten
-    Y = []
-    for x in X:
-        Y.append(X.count(x)/len(X))
-
-    # Histogramm
-    ax.fill_between(X, Y, facecolor='black', step="mid", zorder=30)
-
-    # entferne Überflüssige Daten (besser wäre, sie zu mitteln)
-    new_X = []; new_Y = []
-    for x in X:
-        if x not in new_X:
-            new_X.append(x)
-            new_Y.append(Y[X.index(x)])
-
-    # Shannon-Information
-    I = 0
-    for y in new_Y:
-        I += y * math.log(y, np.e)
-    print(I)
-
+    # Breite der Verteilung
+    # B = abs(min(X)) +  abs(max(X))
+    a += 1
+    ax.hist(X, 9*a, density=True, align="mid", color="black", zorder=30)
 
 # Axes & Grid
-ax.set_ylim(0,0.28)
+ax.set_ylim(0,0.2)
 ax.xaxis.set_major_locator(ticker.MultipleLocator(50))
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(10))
 ax.grid(b=True, color='white', linestyle="--", linewidth=0.5, zorder=0)
@@ -66,9 +47,9 @@ ax.grid(b=True, color='white', linestyle="--", linewidth=0.5, zorder=0)
 # Label & Text
 plt.ylabel("Wahrscheinlichkeit", size=9)
 plt.xlabel(r"Schritte $x$", size=9)
-plt.text(15, 0.25, r"$P_1[x(t=10)]$")
-plt.text(35, 0.10, r"$P_2[x(t=100)]$")
-plt.text(395, 0.045, r"$P_3[x(t=1000)]$")
+plt.text(-1, 0.162, r"$P_1[x(t=10)]$")
+plt.text(35, 0.075, r"$P_2[x(t=100)]$")
+plt.text(395, 0.03, r"$P_3[x(t=1000)]$")
 
 # Render
 plt.tight_layout()
